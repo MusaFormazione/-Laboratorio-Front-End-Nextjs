4e4b7f2dd52dba932f879a43bcdc54ff7549a68a
Davide Bonifacio <web@davidebonifacio.it>
2023-11-21 20:04:46 +0100

    test

--- app/header.js
+++ app/header.js
@@ -19,7 +19,7 @@ export function Header() {
         'bg-teal-500' :
         'bg-violet-600'}`}
       >
-          About us
+          About us!
       </Link>
       <Link href='/products'
         className={`px-4 py-2 ${pathName === '/products' ? 
